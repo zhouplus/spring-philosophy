81beb18a140fb34d88435864dcd26b0b2b462770
mrchang <cjzjava@sina.com>
2020-02-29 20:16:25 +0800

    Soring 容器依赖的三个来源：自定义Bean\容器内建Bean\容器内建依赖（非Bean）

--- thinkinginspring/ioc-container-overview/src/main/java/com/zhou/spring/philosophy/ioc/container/overview/dependency/injection/DependencyInjectionDemo.java
+++ thinkinginspring/ioc-container-overview/src/main/java/com/zhou/spring/philosophy/ioc/container/overview/dependency/injection/DependencyInjectionDemo.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 import java.util.Map;
 
@@ -27,11 +28,11 @@ public class DependencyInjectionDemo {
         //启动 Spring 应用上下文
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
 
-        UserRepository userRepository = (UserRepository) beanFactory.getBean("userRepository",UserRepository.class);
+        //UserRepository userRepository = (UserRepository) beanFactory.getBean("userRepository",UserRepository.class);
        // System.out.println(userRepository.getUsers());
 
         //依赖注入
-        System.out.println(userRepository.getBeanFactory());
+        //System.out.println(userRepository.getBeanFactory());
         //System.out.println(userRepository.getBeanFactory() == beanFactory);
 
 
@@ -45,7 +46,18 @@ public class DependencyInjectionDemo {
         //答案是superUser对象，原因是superUser对象我们定义了 Primary
         //System.out.println(userObjectFactory.getObject());
 
-        ObjectFactory<ApplicationContext> objectFactory = userRepository.getObjectFactory();
-        System.out.println(objectFactory.getObject() == beanFactory);
+//        ObjectFactory<ApplicationContext> objectFactory = userRepository.getObjectFactory();
+////        System.out.println(objectFactory.getObject() == beanFactory);
+
+        //依赖来源一： 自定义 Bean
+        UserRepository userRepository = (UserRepository) beanFactory.getBean("userRepository",UserRepository.class);
+        // System.out.println(userRepository.getUsers());
+
+        //依赖来源二： 依赖注入（内建 依赖）
+        System.out.println(userRepository.getBeanFactory());
+
+        //依赖来源三： 容器内建的 Bean
+        Environment environment = beanFactory.getBean(Environment.class);
+        System.out.println("获取 Environment 类型的Bean -> " + environment);
     }
 }
