d34b79a133750dbd0e596530e135fd392f7ce638
mrchang <cjzjava@sina.com>
2020-02-29 19:12:18 +0800

    IoC依赖查找- 根据 Bean 类型查找，根据类型查找一个集合

--- thinkinginspring/ioc-container-overview/src/main/java/com/zhou/spring/philosophy/ioc/container/overview/dependency/lookup/DependencyLookupDemo.java
+++ thinkinginspring/ioc-container-overview/src/main/java/com/zhou/spring/philosophy/ioc/container/overview/dependency/lookup/DependencyLookupDemo.java
@@ -2,11 +2,19 @@ package com.zhou.spring.philosophy.ioc.container.overview.dependency.lookup;
 
 import com.zhou.spring.philosophy.ioc.container.overview.dependency.domain.User;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Map;
+
 /**
  * 依赖查找示例
+ * 1. 通过名称查找
+ * 2. 通过类型查找
+ *
+ *
+ *
  * Created on 2020/2/29 6:36 下午
  *
  * @author <a href="cjzjava@gmail.com">zhouplus</a>
@@ -20,10 +28,30 @@ public class DependencyLookupDemo {
         //启动 Spring 应用上下文
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-lookup-context.xml");
 
-        lookupInRealTime(beanFactory);
-        lookupInLazyTime(beanFactory);
+        //lookupInRealTime(beanFactory);
+        //lookupInLazyTime(beanFactory);
 
 
+        //按照类型查找
+        lookupByType(beanFactory);
+        //按照类型查找一个集合
+        lookupCollectionByType(beanFactory);
+
+
+    }
+
+    private static void lookupCollectionByType(BeanFactory beanFactory) {
+        if(beanFactory instanceof ListableBeanFactory){
+            ListableBeanFactory listableBeanFactory = (ListableBeanFactory)beanFactory;
+            Map<String, User> beansOfType = listableBeanFactory.getBeansOfType(User.class);
+            System.out.println("按照类型查找一个集合->" + beansOfType);
+        }
+
+    }
+
+    private static void lookupByType(BeanFactory beanFactory) {
+        User user = beanFactory.getBean(User.class);
+        System.out.println("按照类型查找单个:" + user);
     }
 
     private static void lookupInLazyTime(BeanFactory beanFactory) {
